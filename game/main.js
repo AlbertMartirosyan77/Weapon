a5485078ac31bd0e87f5c4ccbeb1859311b02140
AlbertMartirosyan77 <albert.m3@tumo.org>
2023-06-30 17:27:40 +0400

    sax lava

---
+++ game/main.js
@@ -0,0 +1,38 @@
+
+let seconds = 20
+let correctAnswer = 0
+let incorrectAnswer = 0
+
+function getElement(id) {
+    return document.getElementById(id)
+}
+
+function timer () {
+  setTimeout(finish, seconds * 1000);
+    getElement("time").innerHTML = seconds;
+    let countdown = setInterval(function () {
+        seconds--;
+        getElement("time").textContent = seconds;
+        if (seconds <= 0 ) clearInterval(countdown);
+        if (seconds == 10) getElement("time").style.color = "#ffffff";
+    }, 1000)
+}
+function check() {
+    let input;
+    try {
+        input = document.querySelector('input[name = "option"]:checked').value;
+    }catch {
+        //alert("unenq sxal")
+        return;
+    }
+    correctAnswer++;
+    getElement("score").innerHTML = correctAnswer
+    clearInterval(checkInterval);
+}
+function finish(){
+    clearInterval(checkInterval);
+    let percentage = 100;
+    getElement("alertaccuracy").innerHTML = `${percentage}%`
+}
+let checkInterval = setInterval(check, 50)
+timer();
