869b56875fe78e8e9b36ff8a9ba2a41839d0da4d
AlbertMartirosyan77 <albert.m3@tumo.org>
2023-07-04 17:44:45 +0400

    sax tocnia

---
+++ game/countries.js
@@ -0,0 +1,83 @@
+const Weapon = [
+    {
+    "weapons": "WEAPON/download.jpg",
+    "name": "M4A1"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/download-5.jpg",
+    "name": "Spas-12"
+    },
+    {
+    "weapons": "WEAPON/images-3.jpg",
+    "name": "m40"
+    },
+    {
+    "weapons": "WEAPON/images-2.jpg",
+    "name": "mmini uzi"
+    },
+    {
+    "weapons": "WEAPON/SVDDRagunov-10.jpg",
+    "name": "Svd Dragunov"
+    },
+    {
+    "weapons": "WEAPON/images-1.jpg",
+    "name": "UMP-45"
+    },
+    {
+    "weapons": "WEAPON/images.jpg",
+    "name": "Scr-L"
+    },
+    {
+    "weapons": "WEAPON/images-6.jpg",
+    "name": "Desert Eagle"
+    },
+    {
+    "weapons": "WEAPON/",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+    {
+    "weapons": "WEAPON/images-4.jpg",
+    "name": "m1887"
+    },
+];                              
+    
--- game/main.js
+++ game/main.js
@@ -7,10 +7,22 @@ function getElement(id) {
     return document.getElementById(id)
 }
 
+
+function getRandomWeapons(){
+    let raundomItem  = Math.floor(Math.random(Weapon.lenght-1)*10) 
+    
+    console.log(raundomItem)
+    return Weapon [raundomItem]
+}
+function main (){
+    coun = getRandomWeapons();
+    getElement("Weapon").src = coun.weapons;
+}
 function timer () {
   setTimeout(finish, seconds * 1000);
     getElement("time").innerHTML = seconds;
     let countdown = setInterval(function () {
+        main ();
         seconds--;
         getElement("time").textContent = seconds;
         if (seconds <= 0 ) clearInterval(countdown);
